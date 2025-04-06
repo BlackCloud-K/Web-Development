ead9b4caa67a6761ce65c93cf280eb92cf8915d9
BlackCloud-k <niyujian2004@126.com>
2025-04-05 22:45:27 -0400

    fix some bug for hw 7

--- MAMP/webdev/assignment_7/index.php
+++ MAMP/webdev/assignment_7/index.php
@@ -21,7 +21,7 @@
                 margin-top: 10px;
                 padding: 10px;
                 width: 350px;
-                height: 250px;
+                height: 280px;
                 display: flex;
                 flex-direction: column;
                 gap: 10px;
